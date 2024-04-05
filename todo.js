d46caf513ef4df8e895ba6ce435e45f0007df456
varshinisivakumar027 <166004726+varshinisivakumar027@users.noreply.github.com>
2024-04-05 10:03:45 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,10 @@
+function createTask(){
+    let text = document.getElementById("input").value
+    let task = `<div class="task">${text} <button onclick="deleteTask(parentNode)" class="del">Done</button></div>`
+    document.querySelector(".tasks").innerHTML += task;
+    document.getElementById("input").value=""
+};
+
+function deleteTask(parentNode){
+    parentNode.remove()
+}
